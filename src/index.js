696744bff319c8f1bced152f343b84844af22486
Mateus Alexandre <mateus.amartins@hotmail.com>
2022-09-01 00:20:21 -0300

    Create method to deposit in account

--- src/index.js
+++ src/index.js
@@ -39,6 +39,21 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
+app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+  const { customer } = request;
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit"
+  }
+  
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+})
+
 app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
   const { customer } = request;
 
