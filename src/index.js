1afece24a06c48c0ec4b8cbd050b3ad2a7154a21
Mateus Alexandre <mateus.amartins@hotmail.com>
2022-09-01 08:59:07 -0300

    Account withdraw

--- src/index.js
+++ src/index.js
@@ -8,15 +8,27 @@ const customers = [];
 
 function verifyIfExistsAccountCPF(request, response, next) {
   const { cpf } = request.headers;
-  const customer = customers.find(customer => customer.cpf === cpf)
+  const customer = customers.find((customer) => customer.cpf === cpf);
 
   if (!customer) {
-    return response.status(400).json({ error: "Customer not found"});
+    return response.status(400).json({ error: "Customer not found" });
   }
 
   request.customer = customer;
 
   return next();
+}
+
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === "credit") {
+      return acc + operation.amount;
+    }
+
+    return acc - operation.amount;
+  }, 0);
+
+  return balance;
 }
 
 app.post("/account", (request, response) => {
@@ -39,20 +51,39 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
-app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
   const { description, amount } = request.body;
   const { customer } = request;
   const statementOperation = {
     description,
     amount,
     created_at: new Date(),
-    type: "credit"
-  }
-  
+    type: "credit",
+  };
+
   customer.statement.push(statementOperation);
 
   return response.status(201).send();
-})
+});
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return response.status(400).json({ error: "Insufficient funds!" });
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit",
+  };
+
+  customer.statement.push(statementOperation);
+  return response.status(201).send();
+});
 
 app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
   const { customer } = request;
