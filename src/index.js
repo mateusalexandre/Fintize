653492cefedf45dcb73b9a34f3a4bb41448487d4
Mateus Alexandre <mateus.amartins@hotmail.com>
2022-08-31 22:26:20 -0300

    Create account and validate if account already exist

--- src/index.js
+++ src/index.js
@@ -1,4 +1,36 @@
-const express = require('express');
+const express = require("express");
+const { v4: uuid } = require("uuid");
 const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+/**
+ * cpf -> string
+ * name -> string
+ * id -> uuid
+ * statement -> []
+ * */
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists" });
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuid(),
+    statement: []
+  });
+
+  return response.status(201).send();
+});
 
 app.listen("3001");
