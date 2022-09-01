92f779503ab6c5fc8154d4d9c5840cec97cf95b0
Mateus Alexandre <mateus.amartins@hotmail.com>
2022-09-01 14:07:07 -0300

    List bank statement by date

--- src/index.js
+++ src/index.js
@@ -91,4 +91,17 @@ app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
   return response.json(customer.statement);
 });
 
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+  const dateFormat = new Date(date + " 00:00");
+  const statement = customer.statement.filter(
+    (statement) =>
+      statement.created_at.toDateString() ===
+      new Date(dateFormat).toDateString()
+  );
+
+  return response.json(statement);
+});
+
 app.listen("3001");
